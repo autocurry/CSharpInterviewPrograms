9c07a76e306019c206a54adb320af8de3e765e87
autocurry <sandeepsekhar555@gmail.com>
2020-09-30 13:27:43 +0530

    initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CSharpInterviewPrograms
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
